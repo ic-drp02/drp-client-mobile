025be19731237479d12661962931950a8db01b01
Adam Dejl <adam.dejl18@imperial.ac.uk>
2020-05-27 17:49:49 +0200

    Customize Native Base theme

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState, Alert } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 import { AppLoading } from 'expo';
-import{ Container, Header, Left, Body, Right, Button, Icon, Title, Text }from 'native-base';
+import{ StyleProvider, Container, Footer, FooterTab, Content, Header, Left, Body, Right, Button, Icon, Title, Text }from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import commonColor from './native-base-theme/variables/commonColor';
+import getTheme from './native-base-theme/components';
 
 export default function App(props) {
   
@@ -31,24 +33,46 @@ export default function App(props) {
   }
 
   return (
-    <View>
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='arrow-back' />
+      <StyleProvider style={getTheme(commonColor)}>
+        <Container>
+          <Header searchBar>
+            <Left>
+              <Button transparent>
+                <Icon name='menu' />
+              </Button>
+            </Left>
+            <Body>
+              <Title>ICON</Title>
+            </Body>
+            <Right>
+              <Button transparent onPress={() => alert("Searching!")}>
+                <Icon name='search' />
+              </Button>
+            </Right>
+          </Header>
+          <Content>
+            <Button style={styles.logo}>
+              <Text>Ask a question</Text>
             </Button>
-          </Left>
-          <Body>
-            <Title>Hello world!</Title>
-          </Body>
-          <Right>
-            <Button transparent>
-              <Icon name='menu' />
+            <Button>
+              <Text>Post an update</Text>
             </Button>
-          </Right>
-        </Header>
-      </Container>
-    </View>
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button full>
+                <Text>Footer</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      </StyleProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 400,
+    height: 150,
+  },
+});
