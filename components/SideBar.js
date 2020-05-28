68dca7adc05f55d36ba23f133b0c183be0e6c5c2
Adam Dejl <adam.dejl18@imperial.ac.uk>
2020-05-28 14:25:16 +0200

    Tidy up drawer styling, add ICON logo

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,32 +1,29 @@
 import React from "react";
-import { Image } from "react-native";
+import { View, Image } from "react-native";
 import { Container, Content, Text, List, ListItem } from "native-base";
 
 export default function SideBar({ navigation }) {
   return (
-    <Container>
-      <Content>
-        <Image
-          source={require("./splash.jpg")}
-          style={{
-            resizeMode: "stretch",
-            justifyContent: "center",
-            margin: 5,
-            alignItems: "center",
-          }}
-        />
-        <List>
-          <ListItem button onPress={() => navigation.navigate("Home")}>
-            <Text>Home button</Text>
-          </ListItem>
-          <ListItem button onPress={() => navigation.navigate("Question")}>
-            <Text>Ask a question</Text>
-          </ListItem>
-          <ListItem button onPress={() => navigation.navigate("PostUpdate")}>
-            <Text>Post an update</Text>
-          </ListItem>
-        </List>
-      </Content>
-    </Container>
+    <View style={{ width: "100%" }}>
+      <Image
+        source={require("../assets/logo.jpg")}
+        style={{
+          width: "100%",
+          height: 120,
+          resizeMode: "contain",
+        }}
+      />
+      <List>
+        <ListItem button onPress={() => navigation.navigate("Home")}>
+          <Text>Home button</Text>
+        </ListItem>
+        <ListItem button onPress={() => navigation.navigate("Question")}>
+          <Text>Ask a question</Text>
+        </ListItem>
+        <ListItem button onPress={() => navigation.navigate("PostUpdate")}>
+          <Text>Post an update</Text>
+        </ListItem>
+      </List>
+    </View>
   );
 }
