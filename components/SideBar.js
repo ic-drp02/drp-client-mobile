4497b624d0745b9908853a9bac501bc1daa82d4c
Hasan Ali <git@hasali.co.uk>
2020-05-29 18:34:47 +0100

    Improve drawer layout and add search link

--- components/SideBar.js
+++ components/SideBar.js
@@ -2,9 +2,11 @@ import React from "react";
 import { View, Image } from "react-native";
 import { Drawer, Divider } from "react-native-paper";
 
+const DarkDivider = () => <Divider style={{ backgroundColor: "#bbb" }} />;
+
 export default function SideBar({ navigation }) {
   return (
-    <View style={{ width: "100%" }}>
+    <View style={{ flex: 1 }}>
       <View style={{ height: 120, marginTop: 24, padding: 8 }}>
         <Image
           source={require("../assets/logo.jpg")}
@@ -16,27 +18,37 @@ export default function SideBar({ navigation }) {
           resizeMode="contain"
         />
       </View>
-      <Divider style={{ backgroundColor: "#bbb" }} />
-      <Drawer.Item
-        icon="home-variant-outline"
-        label="Home"
-        onPress={() => navigation.navigate("Home")}
-      />
-      <Drawer.Item
-        icon="bell-outline"
-        label="Updates"
-        onPress={() => navigation.navigate("Updates")}
-      />
-      <Drawer.Item
-        icon="help-circle-outline"
-        label="Ask a question"
-        onPress={() => navigation.navigate("Question")}
-      />
-      <Drawer.Item
-        icon="pencil-outline"
-        label="Post an update"
-        onPress={() => navigation.navigate("PostUpdate")}
-      />
+      <DarkDivider />
+      <View style={{ flex: 1 }}>
+        <Drawer.Item
+          icon="home-variant-outline"
+          label="Home"
+          onPress={() => navigation.navigate("Home")}
+        />
+        <Drawer.Item
+          icon="bell-outline"
+          label="Updates"
+          onPress={() => navigation.navigate("Updates")}
+        />
+        <Drawer.Item
+          icon="magnify"
+          label="Search"
+          onPress={() => navigation.navigate("Search")}
+        />
+      </View>
+      <DarkDivider />
+      <View>
+        <Drawer.Item
+          icon="help-circle-outline"
+          label="Ask a question"
+          onPress={() => navigation.navigate("Question")}
+        />
+        <Drawer.Item
+          icon="pencil-outline"
+          label="Post an update"
+          onPress={() => navigation.navigate("PostUpdate")}
+        />
+      </View>
     </View>
   );
 }
