d83d25d49975d8319e5865769ae99887683b40fe
Hasan Ali <git@hasali.co.uk>
2020-05-30 16:39:40 +0100

    Add support for creating posts with tags

--- screens/PostUpdate.js
+++ screens/PostUpdate.js
@@ -6,33 +6,50 @@ import {
   Keyboard,
 } from "react-native";
 
-import { Appbar, TextInput, Button } from "react-native-paper";
+import {
+  Appbar,
+  Button,
+  Chip,
+  TextInput,
+  withTheme,
+  Portal,
+} from "react-native-paper";
+
+import TagPickerDialog from "../components/TagPickerDialog";
 
 import api from "../util/api";
 
-export default function PostUpdate({ navigation }) {
+export default withTheme(function PostUpdate({ navigation, theme }) {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+  const [tagsDialogVisible, setTagsDialogVisible] = useState(false);
 
-  async function submitData(title, summary, content) {
+  async function submitData(title, summary, content, tags) {
     try {
-      const res = await api.createPost({ title, summary, content });
+      const res = await api.createPost({
+        title,
+        summary,
+        content,
+        tags: tags.map((t) => t.name),
+      });
+
       if (!res.success) {
         console.warn("An error occured, status code " + res.status + "!");
         return;
       }
+
       navigation.navigate("UpdatePosted");
     } catch (error) {
       console.warn(error);
     }
   }
 
-  const submitPost = useCallback(() => submitData(title, summary, content), [
-    title,
-    summary,
-    content,
-  ]);
+  const submitPost = useCallback(
+    () => submitData(title, summary, content, tags),
+    [title, summary, content, tags]
+  );
 
   return (
     <View style={{ flex: 1 }}>
@@ -65,6 +82,48 @@ export default function PostUpdate({ navigation }) {
               onChangeText={(text) => setContent(text)}
               style={{ margin: 8 }}
             />
+            <View
+              style={{
+                margin: 8,
+                flexDirection: "row",
+                flexWrap: "wrap",
+              }}
+            >
+              {tags.map((tag) => (
+                <Chip
+                  key={tag.id}
+                  mode="outlined"
+                  onClose={() => setTags(tags.filter((t) => t.id !== tag.id))}
+                  style={{ margin: 4 }}
+                >
+                  {tag.name}
+                </Chip>
+              ))}
+              <Chip
+                icon="plus"
+                mode="outlined"
+                onPress={() => setTagsDialogVisible(true)}
+                style={{ margin: 4, backgroundColor: theme.colors.primary }}
+                theme={{ colors: { text: "#fff" } }}
+              >
+                Add Tag
+              </Chip>
+              <Portal>
+                <TagPickerDialog
+                  visible={tagsDialogVisible}
+                  initialSelected={tags}
+                  onDismiss={() => setTagsDialogVisible(false)}
+                  onTagSelectionChange={(tag) => {
+                    if (tag.selected) {
+                      setTags([...tags, tag]);
+                    } else {
+                      setTags(tags.filter((t) => t.id !== tag.id));
+                    }
+                  }}
+                  style={{ maxHeight: "80%" }}
+                />
+              </Portal>
+            </View>
             <Button
               mode="contained"
               onPress={() => submitPost()}
@@ -77,7 +136,7 @@ export default function PostUpdate({ navigation }) {
       </View>
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
