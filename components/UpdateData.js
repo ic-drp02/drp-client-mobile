5e1b146f48a3dc48f6f2dd9aa2c5c1f8509d7e7b
Hasan Ali <git@hasali.co.uk>
2020-05-30 20:22:35 +0100

    Enable rendering rich text content in posts

--- components/UpdateData.js
+++ components/UpdateData.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { View } from "react-native";
-import { Headline, Text, ProgressBar } from "react-native-paper";
+import React, { useState, useEffect, useRef } from "react";
+import { View, Linking } from "react-native";
+import { Headline, ActivityIndicator } from "react-native-paper";
+import { WebView } from "react-native-webview";
 
-import { getDetails } from "../util/api.js";
-import { COLOR_PRIMARY } from "../util/colors.js";
+import * as api from "../util/api.js";
 
 export default function UpdateData(props) {
   const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(true);
   const id = props.id;
+  const ref = useRef(null);
 
   async function loadPost() {
     try {
-      setData(await getDetails(id));
+      setData(await api.getDetails(id));
     } catch (error) {
       console.warn(error);
     }
@@ -24,13 +26,62 @@ export default function UpdateData(props) {
   }, [data]);
 
   if (data === undefined) {
-    return <ProgressBar color={COLOR_PRIMARY} indeterminate={true} />;
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator indeterminate size="large" />
+      </View>
+    );
   }
 
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>${data.title}</title>
+        <style>
+          html, body {
+            margin: 0;
+          }
+        </style>
+      </head>
+      <body>
+        ${data.content}
+        <script>
+          window.ReactNativeWebView.postMessage("onload");
+        </script>
+      </body>
+    </html>
+  `;
+
   return (
-    <View>
-      <Headline>{data.title}</Headline>
-      <Text>{data.content}</Text>
-    </View>
+    <>
+      {loading && (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ActivityIndicator indeterminate size="large" />
+        </View>
+      )}
+      <View style={{ flex: 1, padding: 8, display: loading ? "none" : "flex" }}>
+        <Headline>{data.title}</Headline>
+        <WebView
+          ref={ref}
+          originWhitelist={["*"]}
+          style={{ backgroundColor: "transparent" }}
+          source={{ html: htmlContent }}
+          onMessage={({ nativeEvent: { data } }) =>
+            data === "onload" && setLoading(false)
+          }
+          onNavigationStateChange={(event) => {
+            if (event.url !== "about:blank") {
+              ref.current.stopLoading();
+              Linking.openURL(event.url);
+            }
+          }}
+        />
+      </View>
+    </>
   );
 }
