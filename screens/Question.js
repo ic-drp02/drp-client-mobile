704d359c2cc1eb2aaf106ac77382c7e662edc6e2
Hasan Ali <git@hasali.co.uk>
2020-06-02 12:08:52 +0100

    Show loading indicators and alert on error

--- screens/Question.js
+++ screens/Question.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useContext } from "react";
 import {
   View,
   Keyboard,
   ScrollView,
   TouchableWithoutFeedback,
 } from "react-native";
-import { Appbar, Button, Text, TextInput } from "react-native-paper";
+
+import {
+  Appbar,
+  Button,
+  Text,
+  TextInput,
+  ProgressBar,
+  Snackbar,
+} from "react-native-paper";
 
 import Dropdown from "../components/Dropdown";
+import SnackbarContext from "../SnackbarContext";
 
 import { Grade } from "drp-api-js";
 import api from "../util/api";
@@ -23,16 +32,27 @@ export default function Question({ navigation }) {
 
   const [site, setSite] = useState(undefined);
   const [grade, setGrade] = useState(Grade.Consultant);
-  const [specialty, setSpecialty] = useState(undefined);
+  const [specialty, setSpecialty] = useState("");
   const [subject, setSubject] = useState(undefined);
-  const [query, setQuery] = useState(undefined);
+  const [query, setQuery] = useState("");
+
+  const [submitting, setSubmitting] = useState(false);
+  const snackbar = useContext(SnackbarContext);
 
   useEffect(() => {
-    api.getSites().then((res) => setSites(res.data));
-    api.getQuestionSubjects().then((res) => setSubjects(res.data));
+    api.getSites().then((res) => {
+      res.data.length > 0 && setSite(res.data[0].id);
+      setSites(res.data);
+    });
+    api.getQuestionSubjects().then((res) => {
+      res.data.length > 0 && setSubject(res.data[0].id);
+      setSubjects(res.data);
+    });
   }, []);
 
   const submitQuestion = useCallback(() => {
+    setSubmitting(true);
+    console.log(site, grade, specialty, subject, query);
     api
       .createQuestions({
         site: sites.find((s) => s.id === site).name,
@@ -46,10 +66,18 @@ export default function Question({ navigation }) {
         ],
       })
       .then((res) => {
+        setSubmitting(false);
         if (res.success) {
           navigation.navigate("Home");
         } else {
-          console.warn("Failed with code " + res.status);
+          snackbar.show(
+            "An error occurred while submitting your questions",
+            Snackbar.DURATION_SHORT,
+            {
+              label: "ok",
+              onPress: (hide) => hide(),
+            }
+          );
         }
       });
   }, [site, specialty, subject, query, sites, subjects]);
@@ -63,77 +91,86 @@ export default function Question({ navigation }) {
         </Appbar.Header>
       </TouchableWithoutFeedback>
       <View style={{ flex: 1 }}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-          <ScrollView>
-            <View style={{ padding: 24 }}>
-              <View style={{ marginVertical: 4 }}>
-                <Text>Which Imperial site are you at?</Text>
-                <Dropdown
-                  items={
-                    !!sites
-                      ? sites.map((site) => ({
-                          label: site.name,
-                          value: site.id,
-                        }))
-                      : []
-                  }
-                  selected={site}
-                  onSelectionChange={(s) => setSite(s)}
-                  style={{ marginVertical: 8 }}
-                />
-              </View>
-              <View style={{ marginVertical: 4 }}>
-                <Text>What is your grade?</Text>
-                <Dropdown
-                  items={grades}
-                  selected={grade}
-                  onSelectionChange={(v) => setGrade(v)}
-                  style={{ marginVertical: 8 }}
-                />
-              </View>
-              <View style={{ marginVertical: 4 }}>
-                <Text>What is your specialty?</Text>
+        {sites === null || subjects === null ? (
+          <ProgressBar indeterminate />
+        ) : (
+          <TouchableWithoutFeedback
+            onPress={Keyboard.dismiss}
+            accessible={false}
+          >
+            <ScrollView>
+              <View style={{ padding: 24 }}>
+                <View style={{ marginVertical: 4 }}>
+                  <Text>Which Imperial site are you at?</Text>
+                  <Dropdown
+                    items={
+                      !!sites
+                        ? sites.map((site) => ({
+                            label: site.name,
+                            value: site.id,
+                          }))
+                        : []
+                    }
+                    selected={site}
+                    onSelectionChange={(s) => setSite(s)}
+                    style={{ marginVertical: 8 }}
+                  />
+                </View>
+                <View style={{ marginVertical: 4 }}>
+                  <Text>What is your grade?</Text>
+                  <Dropdown
+                    items={grades}
+                    selected={grade}
+                    onSelectionChange={(v) => setGrade(v)}
+                    style={{ marginVertical: 8 }}
+                  />
+                </View>
+                <View style={{ marginVertical: 4 }}>
+                  <Text>What is your specialty?</Text>
+                  <TextInput
+                    label="Specialty"
+                    mode="outlined"
+                    onChangeText={(v) => setSpecialty(v)}
+                    style={{ marginVertical: 8 }}
+                  />
+                </View>
+                <View style={{ marginVertical: 4 }}>
+                  <Text>What is your question about?</Text>
+                  <Dropdown
+                    items={
+                      !!subjects
+                        ? subjects.map((subject) => ({
+                            label: subject.name,
+                            value: subject.id,
+                          }))
+                        : []
+                    }
+                    selected={subject}
+                    onSelectionChange={(s) => setSubject(s)}
+                    style={{ marginVertical: 8 }}
+                  />
+                </View>
                 <TextInput
-                  label="Specialty"
+                  label="Query"
                   mode="outlined"
-                  onChangeText={(v) => setSpecialty(v)}
+                  multiline={true}
+                  numberOfLines={7}
+                  onChangeText={(v) => setQuery(v)}
                   style={{ marginVertical: 8 }}
                 />
-              </View>
-              <View style={{ marginVertical: 4 }}>
-                <Text>What is your question about?</Text>
-                <Dropdown
-                  items={
-                    !!subjects
-                      ? subjects.map((subject) => ({
-                          label: subject.name,
-                          value: subject.id,
-                        }))
-                      : []
-                  }
-                  selected={subject}
-                  onSelectionChange={(s) => setSubject(s)}
+                <Button
+                  mode="contained"
+                  onPress={submitQuestion}
+                  disabled={submitting}
+                  loading={submitting}
                   style={{ marginVertical: 8 }}
-                />
+                >
+                  Submit
+                </Button>
               </View>
-              <TextInput
-                label="Query"
-                mode="outlined"
-                multiline={true}
-                numberOfLines={7}
-                onChangeText={(v) => setQuery(v)}
-                style={{ marginVertical: 8 }}
-              />
-              <Button
-                mode="contained"
-                onPress={submitQuestion}
-                style={{ marginVertical: 8 }}
-              >
-                Submit
-              </Button>
-            </View>
-          </ScrollView>
-        </TouchableWithoutFeedback>
+            </ScrollView>
+          </TouchableWithoutFeedback>
+        )}
       </View>
     </View>
   );
