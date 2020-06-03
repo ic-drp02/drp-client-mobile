d0f1f7a18b11441a44183de9af4097dc9e77360d
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2020-06-03 17:18:42 +0700

    Make updates screen scrollable

--- screens/Updates.js
+++ screens/Updates.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { StyleSheet, StatusBar, View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { Appbar } from "react-native-paper";
+import Constants from "expo-constants";
+import { DEFAULT_APPBAR_HEIGHT } from "../node_modules/react-native-paper/src/components/Appbar/Appbar.tsx";
 
 import LatestUpdates from "../components/LatestUpdates.js";
 
 export default function Question({ navigation }) {
+  const viewPadding = 16;
+  const topheight =
+    Constants.statusBarHeight + DEFAULT_APPBAR_HEIGHT + viewPadding;
+
   return (
     <View>
       <Appbar.Header>
         <Appbar.BackAction onPress={() => navigation.goBack()} />
         <Appbar.Content title="All Updates" />
       </Appbar.Header>
-      <View style={{ padding: 16 }}>
+      <ScrollView
+        style={{ padding: viewPadding }}
+        contentContainerStyle={{ paddingBottom: topheight }}
+      >
         <LatestUpdates />
-      </View>
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
