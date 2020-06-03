73604bbb6e2e8dff16ee0f26edeecf63ead1e90d
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2020-06-03 18:57:02 +0700

    Make updates screen scrollable without requiring calculations

--- screens/Updates.js
+++ screens/Updates.js
@@ -1,28 +1,23 @@
 import React from "react";
 import { View, ScrollView } from "react-native";
 import { Appbar } from "react-native-paper";
-import Constants from "expo-constants";
-import { DEFAULT_APPBAR_HEIGHT } from "../node_modules/react-native-paper/src/components/Appbar/Appbar.tsx";
 
 import LatestUpdates from "../components/LatestUpdates.js";
 
 export default function Question({ navigation }) {
-  const viewPadding = 16;
-  const topheight =
-    Constants.statusBarHeight + DEFAULT_APPBAR_HEIGHT + viewPadding;
+  const fullHeight = { flex: 1 };
 
   return (
-    <View>
+    <View style={fullHeight}>
       <Appbar.Header>
         <Appbar.BackAction onPress={() => navigation.goBack()} />
         <Appbar.Content title="All Updates" />
       </Appbar.Header>
-      <ScrollView
-        style={{ padding: viewPadding }}
-        contentContainerStyle={{ paddingBottom: topheight }}
-      >
-        <LatestUpdates />
-      </ScrollView>
+      <View style={fullHeight}>
+        <ScrollView contentContainerStyle={{ padding: 16 }}>
+          <LatestUpdates />
+        </ScrollView>
+      </View>
     </View>
   );
 }
