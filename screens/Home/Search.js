17d1b71e6ce4868fd1d35555ca37a697698351bb
Adam Dejl <adam.dejl18@imperial.ac.uk>
2020-06-04 17:49:35 +0200

    Make entries pressable when keyboard is open

--- screens/Home/Search.js
+++ screens/Home/Search.js
@@ -1,5 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
-import { View, ScrollView } from "react-native";
+import {
+  View,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { Appbar, Searchbar } from "react-native-paper";
 
 import LatestUpdates from "../../components/LatestUpdates.js";
@@ -21,21 +26,28 @@ export default function Search({ navigation }) {
 
   return (
     <View style={fullHeight}>
-      <Appbar.Header>
-        <Appbar.BackAction onPress={() => navigation.goBack()} />
-        <View style={{ flex: 1, marginLeft: 16, marginRight: 8 }}>
-          <Searchbar
-            placeholder="Search"
-            style={{ height: 42 }}
-            ref={ref}
-            onChangeText={(text) => setSearchText(text)}
-          />
-        </View>
-      </Appbar.Header>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={() => navigation.goBack()} />
+          <View style={{ flex: 1, marginLeft: 16, marginRight: 8 }}>
+            <Searchbar
+              placeholder="Search"
+              style={{ height: 42 }}
+              ref={ref}
+              onChangeText={(text) => setSearchText(text)}
+            />
+          </View>
+        </Appbar.Header>
+      </TouchableWithoutFeedback>
       <View style={fullHeight}>
-        <ScrollView contentContainerStyle={{ padding: 16 }}>
-          <LatestUpdates search={searchText} />
-        </ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <ScrollView
+            contentContainerStyle={{ padding: 16 }}
+            keyboardShouldPersistTaps="handled"
+          >
+            <LatestUpdates search={searchText} />
+          </ScrollView>
+        </TouchableWithoutFeedback>
       </View>
     </View>
   );
