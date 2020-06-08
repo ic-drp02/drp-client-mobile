b0d58a3587ac075e55ad5c40b8d0b80f3fb400ed
Hasan Ali <git@hasali.co.uk>
2020-06-08 17:58:28 +0100

    Update public backend url

--- util/api.js
+++ util/api.js
@@ -3,7 +3,7 @@ import ApiClient from "drp-api-js";
 
 const API_SERVER_BASE =
   (Constants.manifest.env && Constants.manifest.env.EXPO_API_SERVER_BASE) ||
-  "https://iconguidelines.hasali.co.uk";
+  "https://icon.doc.ic.ac.uk";
 
 console.log(API_SERVER_BASE);
 
